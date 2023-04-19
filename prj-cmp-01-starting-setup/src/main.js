49bf089bae15ce1d7731ee4c0aae6b5049b6b921
なる <naru.prog.0106@gmail.com>
2023-04-20 08:58:02 +0900

    UIフォルダの中にBaseCard.veuを入れた だがtemplateの中に<div>を入れるとVSCODEにエラーが出る

--- prj-cmp-01-starting-setup/src/main.js
+++ prj-cmp-01-starting-setup/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue'
+import BaseCard from './components/UI/BaseCard.vue';
 
+const app =createApp(App)
+app.component('base-vard',BaseCard)
 createApp(App).mount('#app');
