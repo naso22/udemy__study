3cd4dbd8c59b79b49d3d3769ec87ef1fd93d97e8
なる <naru.prog.0106@gmail.com>
2023-04-19 15:27:37 +0900

    secsion10 128 First Components 終わり

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,3 +1,41 @@
 <template>
-    
+    <ul>
+        <learning-resource v-for="res in storedResources " 
+        :key='res.id' 
+        :title="res.title" 
+        :description="res.description"></learning-resource>
+    </ul>
 </template>
+
+<script>
+import LearningResource from "./components/LearningResource.vue";
+
+export default {
+
+    components:{
+        LearningResource
+    },
+
+    data(){
+        return{
+            storedResources:[
+                {
+                    id: 'official-guide',
+                    title:'official guide',
+                    description:'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+
+                {
+                    id: 'official-guide',
+                    title:'Google',
+                    description:'learn to google',
+                    link: 'https://vuejs.org'
+                },
+
+            ]
+        }
+
+    }
+}
+</script>
---
+++ prj-cmp-01-starting-setup/src/components/LearningResource.vue
@@ -0,0 +1,23 @@
+<template>
+    <li><div>
+        <header>
+            <h3>{{title}}</h3>
+            <button>
+                delete
+                </button>
+                </header>
+            </div>
+            <p>{{description}}</p>
+            <nav>
+                <a :href='link'>Vie resource</a>
+            </nav>
+            </li>
+</template>
+
+
+<script>
+
+export default {
+    props:['title','description','link']
+}
+</script>
