e98bc505056456c13ff0bd8e88e01dfc07ec9a14
なる <naru.prog.0106@gmail.com>
2023-04-19 17:35:30 +0900

    129. Styling and More Components を受講

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,3 +1,53 @@
 <template>
-    
+    <stored-resources :resources="storedResources"></stored-resources>
 </template>
+
+<script>
+import StoredResources from './components/lerning/StoredResources.vue'
+
+export default {
+
+    components:{
+        StoredResources,
+    },
+
+    data(){
+        return{
+            storedResources:[
+                {
+                    id: 'official-guide',
+                    title:'official guide',
+                    description:'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+
+                {
+                    id: 'official-guide',
+                    title:'Google',
+                    description:'learn to google',
+                    link: 'https://vuejs.org'
+                },
+
+            ]
+        }
+
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
+
