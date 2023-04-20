e96ebf73613df4efc2e1ee68d26f08a051d62d08
なる <naru.prog.0106@gmail.com>
2023-04-20 14:24:42 +0900

    secrion10 132　終わり

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,18 +1,17 @@
 <template>
     <the-header title="RememberMe"></the-header>
-    <stored-resources :resources="storedResources"></stored-resources>
+
 </template>
 
 <script>
-import StoredResources from './components/lerning/StoredResources.vue'
+
 import TheHeader from './components/layouts/TheHeader.vue'
 
 
 export default {
 
     components:{
-        StoredResources,
-        TheHeader
+        TheHeader,
     },
 
     data(){
