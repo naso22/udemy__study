3b8612fe95b81f3c7f1f5b968d2984e09af4f59c
なる <naru.prog.0106@gmail.com>
2023-04-20 17:00:42 +0900

    commit

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,17 +1,18 @@
 <template>
     <the-header title="RememberMe"></the-header>
-
+    <the-resources></the-resources>
 </template>
 
 <script>
 
 import TheHeader from './components/layouts/TheHeader.vue'
-
+import TheResources from './components/lerning/TheResources.vue'
 
 export default {
 
     components:{
         TheHeader,
+        TheResources
     },
 
     data(){
