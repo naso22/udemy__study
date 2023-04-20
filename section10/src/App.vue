2334c5f3fadd619ea5ec3caa1267df907cb74199
なる <naru.prog.0106@gmail.com>
2023-04-20 19:31:50 +0900

    10. Course Project: The Learning Resources App 127. Setup & First Steps 完了

---
+++ section10/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data (){
+    return{
+      storedResources:[
+        {
+          id:'official-guide',
+          title:'official guide',
+          description:'the official vue.js documentation.',
+          link:'https://vuejs.org'
+        },
+      ],
+    };
+  },
+};
+
+
+
+</script>
