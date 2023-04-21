3ceddba415ad66bbe3edb50984dea408c00e0afe
naso22 <naru.prog.0106@gmail.com>
2023-04-21 10:53:32 +0900

    section10. 129. Styling and More Components 終了

--- section10/src/App.vue
+++ section10/src/App.vue
@@ -1,21 +1,12 @@
 <template>
-  <ul>
-    <li>
-    <learning-resource v-for='res in storedResources'
-                       :key='res.id'
-                       :title='res.title'
-                       :description='res.description'
-                       :link='res.link'
-    ></learning-resource>
-    </li>
-  </ul>
+  <stored-resources :resources='storedResources'></stored-resources>
 </template>
 
 <script>
-import LearningResource from './components/lerning-resoures/LearningResource.vue'
+import StoredResources from './components/lerning-resoures/StoredResources.vue'
 export default {
   components:{
-    LearningResource,
+    StoredResources,
   },
   data (){
     return{
@@ -40,3 +31,19 @@ export default {
 
 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
