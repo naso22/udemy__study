128782bed4077fee0dc6dcd3069f91bdf4749149
naso22 <naru.prog.0106@gmail.com>
2023-04-21 10:01:57 +0900

    section10 128. First Components & Props 完了

--- section10/src/App.vue
+++ section10/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <ul></ul>
+  <ul>
+    <li>
+    <learning-resource v-for='res in storedResources'
+                       :key='res.id'
+                       :title='res.title'
+                       :description='res.description'
+                       :link='res.link'
+    ></learning-resource>
+    </li>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/lerning-resoures/LearningResource.vue'
 export default {
+  components:{
+    LearningResource,
+  },
   data (){
     return{
       storedResources:[
@@ -11,6 +24,12 @@ export default {
           id:'official-guide',
           title:'official guide',
           description:'the official vue.js documentation.',
+          link:'https://vuejs.org'
+        },
+        {
+          id:'Google',
+          title:'Google',
+          description:'...',
           link:'https://vuejs.org'
         },
       ],
---
+++ section10/src/components/lerning-resoures/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+    <header>
+      <h3>{{title}}</h3>
+      <button>delete</button>
+    </header>
+    </div>
+    <p>{{description}}</p>
+    <a href>view resource</a>
+    <nav></nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props :['title','description','link'],
+
+}
+</script>
