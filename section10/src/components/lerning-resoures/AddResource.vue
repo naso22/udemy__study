1cf50056c82f915eb516bc637082af5c49839338
naso22 <naru.prog.0106@gmail.com>
2023-04-21 15:07:27 +0900

    10.134. Adding a Form 終了

--- section10/src/components/lerning-resoures/AddResource.vue
+++ section10/src/components/lerning-resoures/AddResource.vue
@@ -1,3 +1,57 @@
 <template>
-  <h2>AddResource</h2>
+
+  <base-card>
+    <div class='form-control'>
+    <form>
+      <label for='title'>Title</label>
+      <input id='title' name='title' type='text'>
+    </form>
+    </div>
+    <div class='form-control'>
+      <form>
+        <label for='title'>Title</label>
+        <textarea id='description' name='description' rows='3'></textarea>
+      </form>
+    </div>
+    <div class='form-control'>
+      <form>
+        <label for='link'>Title</label>
+        <textarea id='link' name='link' rows='3'></textarea>
+      </form>
+    </div>
+    <div>
+      <base-button type='submit'>Add Resource</base-button>
+    </div>
+  </base-card>
 </template>
+<script setup>
+import BaseButton from '@/components/UI/BaseButton.vue';
+</script>
+
+<style scoped>
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+    outline: none;
+    border-color: #3a0061;
+    background-color: #f7ebff;
+}
+
+.form-control {
+    margin: 1rem 0;
+}
+</style>
