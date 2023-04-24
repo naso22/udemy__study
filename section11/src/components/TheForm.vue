c1b4edad497bfacbaed0ba2efe25a2f7447ff444
naso22 <naru.prog.0106@gmail.com>
2023-04-24 11:37:13 +0900

    section11. Forms142. Working with v-model Modifiers and Numbers 完了 model とresfのデータの受けとり方の違いの勉強

--- section11/src/components/TheForm.vue
+++ section11/src/components/TheForm.vue
@@ -1,12 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent='submitForm'>
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model='userName' />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" ref='ageInput' v-model='userAge'/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -51,6 +51,25 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      userName:'',
+      userAge:null
+    }
+  },
+  methods :{
+    submitForm(){
+      console.log('Username:'+ this.userName);
+      console.log(this.$refs.ageInput.value +5);
+      console.log(this.userAge +5);
+
+    },
+  }
+}
+</script>
 
 <style scoped>
 form {
