5a94796767a843def46c0c8ba9a09f0145130fcf
naso22 <naru.prog.0106@gmail.com>
2023-04-25 11:30:27 +0900

    section12. Sending Http Requests161. Handling Technical / Browser-side Errors エラーした時の挙動を追加

--- section12/src/components/survey/UserExperiences.vue
+++ section12/src/components/survey/UserExperiences.vue
@@ -6,8 +6,13 @@
         <base-button @click='loadExperiences'>Load Submitted Experiences</base-button>
       </div>
       <p v-if='isLoading'>Loading....</p>
+      <p v-else-if="!isLoading && error">
+        {{error}}
+      </p>
       <p v-else-if='!isLoading && (!results || results.length ===0)'>no store experience</p>
-      <ul v-else-if="!isLoading && results && results.length>0">
+
+
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,12 +34,14 @@ export default {
   data(){
     return {
       results: [],
-      isLoading :false
+      isLoading :false,
+      error: null,
     }
   },
   methods:{
     loadExperiences(){
       this.isLoading = true;
+      this.error =null;
       fetch('https://vue-udemy-c1f09-default-rtdb.firebaseio.com/surveys.json')
         .then((response)=>{
         if (response.ok){
@@ -52,7 +59,12 @@ export default {
           });
         }
         this.results =results
-      });
+      })
+        .catch((error) => {
+              console.log(error)
+              this.isLoading =false;
+          this.error ='failed to fetch data - please try again later.'
+        })
       }
   },
 
