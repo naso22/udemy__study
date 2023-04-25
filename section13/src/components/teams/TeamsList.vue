53ea9b98bba717d3700fce85e4fb3ed8c4bd1f5b
naso22 <naru.prog.0106@gmail.com>
2023-04-25 15:51:06 +0900

    section13. Routing: Building "Multi-Page" Single Page Applications173 完了 ボタンでのチームメンバー表示

--- section13/src/components/teams/TeamsList.vue
+++ section13/src/components/teams/TeamsList.vue
@@ -3,6 +3,7 @@
     <teams-item
       v-for="team in teams"
       :key="team.id"
+      :id='team.id'
       :name="team.name"
       :member-count="team.members.length"
     ></teams-item>
@@ -11,6 +12,7 @@
 
 <script>
 import TeamsItem from './TeamsItem.vue';
+
 
 export default {
   components: {
