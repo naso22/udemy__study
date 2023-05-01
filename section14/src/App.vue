ad7fb80e4a62a848e18a489d8375e2e00b88f6c3
naso22 <naru.prog.0106@gmail.com>
2023-05-01 09:14:15 +0900

    新しいボタンとブロックの追加

--- section14/src/App.vue
+++ section14/src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="container">
-    <div class="block" :class="{ animate: animatedBlock }"></div>
-    <button @click="animateBlock">Animate</button>
+  <div class='container'>
+  <users-list></users-list>
   </div>
-  <div class="container">
+  <div class='container'>
+    <div class='block' :class='{ animate: animatedBlock }'></div>
+    <button @click='animateBlock'>Animate</button>
+  </div>
+  <div class='container'>
     <transition
       :css='false'
-      @before-enter="beforeEnter"
-      @enter="enter"
+      @before-enter='beforeEnter'
+      @enter='enter'
       @after-enter='afterEnter'
       @before-leave='afterLeave'
       @leave='leave'
@@ -15,78 +18,81 @@
       @enter-cancelled='enterCancelled'
       @leave-cancelled='LeaveCancelled'
     >
-      <p v-if="paraIsVisible">this is only some...</p>
+      <p v-if='paraIsVisible'>this is only some...</p>
     </transition>
-    <button @click="toggleParagraph">toggle paragraph</button>
+    <button @click='toggleParagraph'>toggle paragraph</button>
   </div>
 
-  <div class="container">
-    <transition name="fade-button" mode="out-in">
-      <button @click="showUsers" v-if="!usersAreVisible">show users</button>
-      <button @click="showHide" v-else>hide users</button>
+  <div class='container'>
+    <transition name='fade-button' mode='out-in'>
+      <button @click='showUsers' v-if='!usersAreVisible'>show users</button>
+      <button @click='showHide' v-else>hide users</button>
     </transition>
   </div>
-  <base-modal @close="hideDialog" :open="dialogIsVisible">
+  <base-modal @close='hideDialog' :open='dialogIsVisible'>
     <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
+    <button @click='hideDialog'>Close it!</button>
   </base-modal>
-  <div class="container">
-    <button @click="showDialog">Show Dialog</button>
+  <div class='container'>
+    <button @click='showDialog'>Show Dialog</button>
   </div>
 </template>
 
 <script>
+import UsersList  from './components/UserList.vue'
 export default {
+  components:{
+    UsersList
+  },
   data() {
     return {
       dialogIsVisible: false,
       animatedBlock: false,
       paraIsVisible: false,
-      usersAreVisible: false,
+      usersAreVisible: false
     };
   },
   methods: {
-    enterCancelled(el){
+    enterCancelled(el) {
       clearInterval(this.enterInterval);
-      console.log(el)
+      console.log(el);
     },
 
-    LeaveCancelled(el){
+    LeaveCancelled(el) {
       clearInterval(this.leaveInterval);
-      console.log(el)
+      console.log(el);
     },
-    enter(el,done){
+    enter(el, done) {
       console.log('enter');
       console.log(el);
-      let round =1;
-      this.enterInterval =setInterval(()=>{
+      let round = 1;
+      this.enterInterval = setInterval(() => {
         el.style.opacity = round * 0.01;
         round++;
-        if (round > 100){
+        if (round > 100) {
           clearInterval(this.enterInterval);
           done();
         }
-      },20)
+      }, 20);
     },
 
-    beforeEnter(el){
-      el.style.opacity=0
+    beforeEnter(el) {
+      el.style.opacity = 0;
     },
 
 
-
-    leave(el,done){
+    leave(el, done) {
       console.log('leave');
       console.log(el);
-      let round =1;
-      this.leaveInterval =setInterval(()=>{
-        el.style.opacity =1- round * 0.01;
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
         round++;
-        if (round > 100){
+        if (round > 100) {
           clearInterval(this.leaveInterval);
           done();
         }
-      },20)
+      }, 20);
     },
     showDialog() {
       this.dialogIsVisible = true;
@@ -109,81 +115,87 @@ export default {
 
     showHide() {
       this.usersAreVisible = false;
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
+
 html {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
+
 body {
-  margin: 0;
+    margin: 0;
 }
+
 button {
-  font: inherit;
-  padding: 0.5rem 2rem;
-  border: 1px solid #810032;
-  border-radius: 30px;
-  background-color: #810032;
-  color: white;
-  cursor: pointer;
+    font: inherit;
+    padding: 0.5rem 2rem;
+    border: 1px solid #810032;
+    border-radius: 30px;
+    background-color: #810032;
+    color: white;
+    cursor: pointer;
 }
+
 button:hover,
 button:active {
-  background-color: #a80b48;
-  border-color: #a80b48;
+    background-color: #a80b48;
+    border-color: #a80b48;
 }
+
 .block {
-  width: 8rem;
-  height: 8rem;
-  background-color: #290033;
-  margin-bottom: 2rem;
+    width: 8rem;
+    height: 8rem;
+    background-color: #290033;
+    margin-bottom: 2rem;
 }
+
 .container {
-  max-width: 40rem;
-  margin: 2rem auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 2rem;
-  border: 2px solid #ccc;
-  border-radius: 12px;
+    max-width: 40rem;
+    margin: 2rem auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 2rem;
+    border: 2px solid #ccc;
+    border-radius: 12px;
 }
 
 .fade-button-enter-from,
 .fade-button-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 
 .fade-button-enter-active {
-  transition: opacity 0.3s ease-out;
+    transition: opacity 0.3s ease-out;
 }
 
 .fade-button-leave-active {
-  transition: opacity 0.3s ease-in;
+    transition: opacity 0.3s ease-in;
 }
 
 .fade-button-enter-from {
-  opacity: 1;
+    opacity: 1;
 }
 
 @keyframes slide-scale {
-  0% {
-    transform: translateX(0) scale(1);
-  }
+    0% {
+        transform: translateX(0) scale(1);
+    }
 
-  70% {
-    transform: translateX(-120px) scale(1.1);
-  }
+    70% {
+        transform: translateX(-120px) scale(1.1);
+    }
 
-  100% {
-    transform: translateX(-150px) scale(1);
-  }
+    100% {
+        transform: translateX(-150px) scale(1);
+    }
 }
 </style>
