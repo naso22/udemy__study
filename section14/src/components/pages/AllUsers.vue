00f7c0d2d1fd0b2f35568eeb24b7b1a77aeee0d0
naso22 <naru.prog.0106@gmail.com>
2023-05-01 14:11:26 +0900

    section15フォルダー

--- section14/src/components/pages/AllUsers.vue
+++ section14/src/components/pages/AllUsers.vue
@@ -1,8 +1,8 @@
 <template>
   <div class='container'>
-    <h2>all users</h2>
+    <h2>all <br>users</h2>
     <router-link to='/goals'>goals</router-link>
   </div>
 </template>
-<script setup>
+<script>
 </script>
