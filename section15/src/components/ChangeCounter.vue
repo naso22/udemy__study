d8e34333b296e66ddeb4f1b61e4ac43c24d9b119
naso22 <naru.prog.0106@gmail.com>
2023-05-01 15:09:21 +0900

    section15 215 ミューテーションを使ったボタン操作

---
+++ section15/src/components/ChangeCounter.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="addOne">Add1</button>
+</template>
+
+<script>
+export default {
+    methods:{
+        addOne(){
+            this.$store.commit('increment')
+        }
+    }
+}
+</script>
