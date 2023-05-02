5800731a456cf58fa02381861fe522763f88ef55
naso22 <naru.prog.0106@gmail.com>
2023-05-02 09:31:33 +0900

    section15 217 ディスパッチの追加

--- section15/src/main.js
+++ section15/src/main.js
@@ -11,10 +11,22 @@ const store = createStore({
     },
     mutations:{
         increment(state){
-            state.counter =state.counter +2;
+            setTimeout(function (){
+                state.counter =state.counter +2;
+            },2000)
         },
         increase(state,payload){
             state.counter =state.counter +payload.value
+        }
+    },
+    actions:{
+        increment(context){
+            setTimeout(function (){
+                context.commit('increment')
+            })
+        },
+        increase(context,payload){
+            context.commit('increase',payload)
         }
     },
     getters:{
