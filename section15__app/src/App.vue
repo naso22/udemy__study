375fbc08e2033f347eda066da0b0d08d42d990b5
naso22 <naru.prog.0106@gmail.com>
2023-05-08 10:31:51 +0900

    section15 227　終了

---
+++ section15__app/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  provide() {
+    return {
+      isLoggedIn: this.isLoggedIn,
+      cart: this.cart,
+      addProductToCart: this.addProductToCart,
+      removeProductFromCart: this.removeProductFromCart,
+      login: this.login,
+      logout: this.logout,
+    };
+  },
+  methods: {
+    login() {
+      this.isLoggedIn = true;
+    },
+    logout() {
+      this.isLoggedIn = false;
+    },
+  },
+  getters:{
+    products(state){
+    return state.products;
+}
+}
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
