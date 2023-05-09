06689f79c6c97e3d0704c29fdd8cb6c83d54c0c0
naso22 <naru.prog.0106@gmail.com>
2023-05-09 09:52:19 +0900

    section16 240 リストアイテムの表示

--- section16/src/pages/coaches/CoachesList.vue
+++ section16/src/pages/coaches/CoachesList.vue
@@ -5,12 +5,17 @@
   <section>
     <div class='controls'>
       <button>Refresh</button>
-      <router-link to='/register' >Register as Coach</router-link>
+      <router-link to='/register'>Register as Coach</router-link>
     </div>
     <ul v-if='hasCoaches'>
-      <li v-for='coach in filteredCoaches' :key='coach.id'>
-        {{coach.firstName}}
-      </li>
+      <coach-item
+        v-for='coach in filteredCoaches'
+        :key='coach.id'
+        :id='coach.id'
+        :first-name='coach.firstName'
+        :last-name='coach.lastName'
+        :rate='coach.hourlyRate'
+        :areas=coach.areas></coach-item>
     </ul>
     <h3 v-else>No coaches found</h3>
   </section>
@@ -18,14 +23,32 @@
 
 
 <script>
-export default{
-  computed:{
-    filteredCoaches(){
-      return this.$store.getters['coaches/coaches']
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
+export default {
+  components: {
+    CoachItem
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
     },
-    hasCoaches(){
-      return this.$store.getters['coaches/hesCoaches']
+    hasCoaches() {
+      return this.$store.getters['coaches/hesCoaches'];
     }
   }
-}
+};
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
